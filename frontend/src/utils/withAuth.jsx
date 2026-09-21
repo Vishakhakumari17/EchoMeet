fa2595c3482bee16c708d2106aa090d6078f729a
Vishakha kumari <vishakhaguptahjp@gmail.com>
2026-09-21 22:31:45 +0530

    fix final eslint warning

--- frontend/src/utils/withAuth.jsx
+++ frontend/src/utils/withAuth.jsx
@@ -5,13 +5,6 @@ const withAuth = (WrappedComponent ) => {
     const AuthComponent = (props) => {
         const router = useNavigate();
 
-        const isAuthenticated = () => {
-            if(localStorage.getItem("token")) {
-                return true;
-            } 
-            return false;
-        }
-
         useEffect(() => {
             if (!localStorage.getItem("token")) {
                 router("/login");
