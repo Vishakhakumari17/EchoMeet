bae8cc367884177e21e5101ea8c306ccb776ba05
Vishakha kumari <vishakhaguptahjp@gmail.com>
2026-09-21 23:29:56 +0530

    fix eslint warning to allow vercel build

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -61,6 +61,7 @@ export default function VideoMeetComponent() {
     useEffect(() => {
         console.log("HELLO")
         getPermissions();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     let getDislayMedia = () => {
